be6d8f64306378b79d0f5779440de91c8a8b0430
RomaNegi <romanegi20@gmail.com>
2016-11-07 21:32:38 +0530

    api for hospital

--- api/hospital.php
+++ api/hospital.php
@@ -2,4 +2,23 @@
 
 //api for hopistal list
 
+//include 'config.php';
+$conn = mysqli_connect('localhost:8889', 'root', 'root', 'myhospital');
+
+
+$sql = "SELECT * FROM Hospital";
+
+$result = mysqli_query($conn, $sql);
+
+$json_array = array();
+
+while($row = mysqli_fetch_assoc($result))
+{
+	$json_array[] = $row;
+
+}
+
+//print_r($json_array);
+
+echo json_encode($json_array);
 ?>
